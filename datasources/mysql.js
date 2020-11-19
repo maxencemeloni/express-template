66d37ecd858d73580e53701befa32f2eda50baf0
Maxence <maxence.meloni@wildcodeschool.com>
2020-11-19 23:46:26 +0100

    delete mysql branch's files

--- datasources/mysql.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql');
-require('dotenv').config();
-module.exports = mysql.createPool({
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    host: process.env.MYSQL_HOST,
-    database: process.env.MYSQL_DATABASE
-});
