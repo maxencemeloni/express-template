0e5a89f6384f9ee7a78530a582482c03235bba6a
Maxence <maxence.meloni@wildcodeschool.com>
2020-11-19 17:45:21 +0100

    readme add mysql

---
+++ key-generator.js
@@ -0,0 +1,16 @@
+const { generateKeyPair } = require('crypto');
+generateKeyPair('rsa', {
+    modulusLength: 4096,
+    publicKeyEncoding: {
+        type: 'spki',
+        format: 'pem'
+    },
+    privateKeyEncoding: {
+        type: 'pkcs8',
+        format: 'pem',
+        cipher: 'aes-256-cbc',
+        passphrase: ''
+    }
+}, (err, publicKey, privateKey) => {
+    // Handle errors and use the generated key pair.
+});
