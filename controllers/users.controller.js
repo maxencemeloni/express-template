93948b8be641d8a6998e94a6b55f41acead5b80d
Maxence <maxence.meloni@wildcodeschool.com>
2020-11-26 19:25:48 +0100

    rollback after failed rebase

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,81 +1,76 @@
-const Users = require('../models/users.model')
-const logger = require('../library/logger')
-const {buildLimit} = require('../utils/controllers');
-const {generatePassword} = require('../utils/users');
+const Users = require('../models/users.model');
+const logger = require('../library/logger');
 const {validationResult} = require('express-validator');
+const {sendResponse, buildLimit} = require('../utils/controllers');
+const {generatePassword} = require('../utils/users');
 
 class UsersController {
-    static async find(req, res) {
-        // /users?limit=10
-        // /users?offset=50
-        // /users?limit=10&offset=50
-        // ******************
-        // warning : the default limit is 10
-        // see => /utils/controllers.js
-        let limits = buildLimit(req, 10);
-        try {
-            let result = await Users.read({}, limits);
-            res.send(result);
-        } catch(err) {
-            logger.error(err);
-            res.sendStatus(500);
-        }
+    // /users?limit=10
+    // /users?offset=50
+    // /users?limit=10&offset=50
+    // ******************
+    // warning : the default limit is 10
+    // see => /utils/controllers.js
+
+    static find(req, res) {
+        const limits = buildLimit(req, 100);
+        Users.read({}, limits, (err, results) => {
+            sendResponse(res, err, results);
+        })
     }
 
-    static async findById(req, res) {
-        try {
-            let result = await Users.read({id: req.params.id}, [1]);
-            res.send(result);
-        } catch(err) {
-            logger.error(err);
-            res.sendStatus(500);
-        }
+    static findById(req, res) {
+        Users.read({id: req.params.id}, [1], (err, results) => {
+            sendResponse(res, err, results);
+        })
     }
 
-    static async createOne(req, res) {
-        const errors = validationResult(req)
+    static createOne(req, res) {
+        const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() })
+            return res.status(400).json({errors: errors.array()});
         }
-
-        try {
-            let data = {
-                email: req.body.email,
-                password: generatePassword(req.body.password)
+        let data = {
+            email: req.body.email,
+            password: generatePassword(req.body.password)
+        }
+        Users.create(data, (err, result) => {
+            if (err) {
+                logger.error(err);
+                res.sendStatus(500);
+            } else {
+                res.sendStatus(201);
             }
-            let result = await Users.create(data);
-            res.sendStatus(201);
-        } catch(err) {
-            logger.error(err);
-            res.sendStatus(500);
-        }
+        })
     }
 
-    static async updateOne(req, res) {
-        let data = req.body;
-        const errors = validationResult(req)
+    static updateOne(req, res) {
+        const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() })
+            return res.status(400).json({errors: errors.array()});
         }
 
-        if(req.body.password !== undefined) {
+        let data = {};
+        if (req.body.email !== undefined) {
+            data.email = req.body.email;
+        }
+        if (req.body.password !== undefined) {
             data.password = generatePassword(req.body.password);
         }
 
-        try {
-            let result = await Users.update(req.params.id, data);
-            if (result.affectedRows > 0) {
-                res.send(result);
-            } else {
+        Users.update(req.params.id, data, (err, result) => {
+            if (err) {
+                logger.error(err);
+                res.sendStatus(500);
+            } else if (result.affectedRows === 0) {
                 res.status(404).send({error: 'Nothing updated'})
+            } else {
+                res.send(result);
             }
-        } catch(err) {
-            logger.error(err);
-            res.sendStatus(500);
-        }
+        });
     }
 
-    static async deleteOne(req, res) {
+    static deleteOne(req, res) {
         // you have enough examples and i'm lazy ;)
         // in fact, i've not implement this method because it depend about your deletion policy
         // you can update and set a field as disabled_at or is_active OR delete the row
